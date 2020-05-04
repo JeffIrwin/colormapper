ffa61ff6620e22ebb351c98750ac209518e11c4e
Jeff Irwin <jirwin505@gmail.com>
2020-05-03 20:41:08 -0400

    Add files via necromancy

---
+++ colormapper_constants.h
@@ -0,0 +1,13 @@
+
+#ifndef INCLUDE_COLORMAPPER_CONSTANTS_H_
+#define INCLUDE_COLORMAPPER_CONSTANTS_H_
+
+const double pi = 4.0 * atan(1.0);
+
+// Error return codes
+const int ERR_JSON      = -2;
+const int ERR_XML_PARSE = -4;
+const int ERR_FILETYPE  = -6;
+
+#endif  // INCLUDE_COLORMAPPER_CONSTANTS_H_
+
